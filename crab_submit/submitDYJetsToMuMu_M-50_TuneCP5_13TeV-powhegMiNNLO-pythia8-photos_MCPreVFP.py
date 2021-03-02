6a081d284c55ebc33d5fdb63c4ffeeaf9f1e235e
Kenneth Long <kdlong@hep.wisc.edu>
2021-03-02 20:17:30 +0100

    Make configs for weightFix samples

---
+++ crab_submit/submitDYJetsToMuMu_M-50_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos_MCPreVFP.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'DYJetsToMuMu_M-50_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos_MCPreVFP'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.numCores = 4
+config.JobType.maxMemoryMB = 8000
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../configs/NanoV8MCPreVFP_weightFix_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/DYJetsToMuMu_M-50_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/mseidel-LHE_massWeights_APVpreVFP-883f8224005bb85ca71ea2ca271fa8bd/USER'
+config.Data.secondaryInputDataset = '/DYJetsToMuMu_M-50_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL16MiniAODAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/MINIAODSIM'
+
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 4
+config.Data.outLFNDirBase = '/store/group/cmst3/group/wmass/w-mass-13TeV/NanoAOD' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'NanoV8MCPreVFP'
+
+config.Site.storageSite = 'T2_CH_CERN'
