d464f8d121180c1c0b2f34270adfb275d8f313fb
Kenneth Long <kdlong@hep.wisc.edu>
2021-11-19 13:31:51 +0100

    Update setup to use sparse checkout, update lhapdf

--- setup/clone.sh
+++ setup/clone.sh
@@ -1,13 +1,25 @@
 #/bin/bash
 set -e
+/cvmfs/cms.cern.ch/cmsswet_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc700
 cmssw=CMSSW_10_6_19_patch2
 scramv1 project $cmssw
 cd $cmssw/src
-scramv1 runtime -sh
-git cms-merge-topic WMass:WmassNanoProd_106X
+eval `scramv1 runtime -sh`
+git cms-init
+git remote add WMass git@github.com:WMass/cmssw.git
+git fetch WMass WmassNanoProd_106X_genweights
+git checkout WMass/WmassNanoProd_106X_genweights
+git checkout -b WmassNanoProd_106X_genWeights 
 path=Configuration/WMassNanoProduction
 git clone git@github.com:WMass/WMassNanoProduction.git $path
-scram b -j8
+cp $path/setup/sparse-checkout .git/info
+# A "trick" to link to the newest LHAPDF
+scram tool remove lhapdf
+cp $path/setup/lhapdf.xml ../config/toolbox/slc7_amd64_gcc700/tools/selected/
+scram setup lhapdf
+git read-tree -mu HEAD
+pwd
+scram b -j24
 
 cd $path
