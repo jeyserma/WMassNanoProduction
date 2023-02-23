a805efa05d4b45447a4d25003a73d9ba0d92ec5c
Kenneth Long <kdlong@hep.wisc.edu>
2023-02-23 10:22:39 +0100

    Submit scripts for MC with fixed PDFs

--- crab_submit/submitWplusJetsToTauNu_TauToMu_H2ErratumFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos_MCPostVFP.py
+++ crab_submit/submitWplusJetsToTauNu_TauToMu_H2ErratumFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos_MCPostVFP.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'WplusJetsToTauNu_TauToMu_H2ErratumFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos_MCPostVFP_Tracka4b46'
+config.General.requestName = 'WplusJetsToTauNu_TauToMu_H2ErratumFix_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos_MCPostVFP_Track4e72f'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -18,7 +18,7 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 2
 config.Data.outLFNDirBase = '/store/group/cmst3/group/wmass/w-mass-13TeV/NanoAOD' 
 config.Data.publication = True
-config.Data.outputDatasetTag = 'NanoV9MCPostVFP_TrackFitV718_NanoProdv1'
+config.Data.outputDatasetTag = 'NanoV9MCPostVFP_TrackFitV722_NanoProdv1'
 config.Data.inputDBS = 'global'
 config.Data.useParent = False
 
